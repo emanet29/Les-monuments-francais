c7a1619b04f4fe017dfaea9453538630220af5a9
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-07 12:27:53 +0200

    obtenirDonneesDepuisJSON()

---
+++ Les monuments francais/Model/Monument.swift
@@ -0,0 +1,16 @@
+//
+//  Monument.swift
+//  Les monuments francais
+//
+//  Created by Snoopy on 07/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import Foundation
+
+struct Monument: Decodable {
+    var name: String?
+    var latitude: String?
+    var longitude: String?
+    
+}
